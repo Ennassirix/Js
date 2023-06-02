9324eafba1db179e8cab4123e0424140b9cb05cf
Ennassirix <ayoubx123456@gmail.com>
2023-06-02 15:21:56 +0100

    add the array

--- main.js
+++ main.js
@@ -84,3 +84,74 @@ const ValidDate = ( date ) => {
     let dateRegex = /^\d{2}\/\d{2}\/\d{4}$/;
     return dateRegex.test( date );
 };
+
+
+// ex 2 
+
+function getData() {
+    // Remplir un tableau de 9 chiffres de 1 à 9
+    var T = [];
+    for ( var i = 1; i <= 9; i++ ) {
+        T.push( i );
+    }
+
+    // Supprimer la valeur 9 du tableau
+    var index = T.indexOf( 9 );
+    if ( index !== -1 ) {
+        T.splice( index, 1 );
+    }
+
+    // Mélanger les éléments du tableau
+    for ( var i = T.length - 1; i > 0; i-- ) {
+        var j = Math.floor( Math.random() * ( i + 1 ) );
+        var temp = T[ i ];
+        T[ i ] = T[ j ];
+        T[ j ] = temp;
+    }
+
+    // Copier le tableau T dans un tableau à 2D de 3 lignes et 3 colonnes
+    var T2D = [];
+    for ( var i = 0; i < 3; i++ ) {
+        T2D.push( T.slice( i * 3, ( i + 1 ) * 3 ) );
+    }
+
+    return T2D;
+}
+
+console.log( getData() );
+
+let table = document.getElementById( 'myTable' );
+console.log(table);
+
+const init = () => {
+    let tr1 = document.createElement( 'tr' );
+    let tr2 = document.createElement( 'tr' );
+    let tr3 = document.createElement( 'tr' );
+    let data = getData();
+
+    data.forEach( ( el ) => {
+        for ( let i = 0; i < 3; i++ ) {
+            let td = document.createElement( 'td' );
+            let tdText = document.createTextNode( el[ i ] );
+            td.appendChild( tdText );
+            if ( i === 0 ) {
+                tr1.appendChild( td );
+            } else if ( i === 1 ) {
+                tr2.appendChild( td );
+            } else {
+                tr3.appendChild( td );
+            }
+        }
+    } );
+
+    // let table = document.createElement( 'table' );
+    table.appendChild( tr1 );
+    table.appendChild( tr2 );
+    table.appendChild( tr3 );
+
+    // document.body.appendChild( table ) ;
+}
+
+console.log( init() );
+
+
